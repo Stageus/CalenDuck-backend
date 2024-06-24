7215861bacde9b5b815a937fac0322884e1cfd1b
yunseo <yunseo10987@naver.com>
2024-06-24 15:03:56 +0900

    feat: "errorHandler 기능 추가"
    errorHandler 미들웨어 생성

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,17 @@
+const { CustomError } = require("./Error");
+
+const errorHandler = async (err, req, res, next) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({
+      message: err.message,
+    });
+  } else {
+    console.log(`에러 발생 : ${err}`);
+
+    return res.status(500).send({
+      message: "서버 에러 발생",
+    });
+  }
+};
+
+module.exports = errorHandler;
