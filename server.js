9be68fbeed8f94a34d578491159c22c2d2e86e6d
SuinLee21 <suin7473@naver.com>
2024-06-25 15:26:56 +0900

    feat: "router 추가"

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
 const express = require("express");
-const app = express();
 
+const errorHandler = require("./src/middlewares/errorHandler");
+const interceptor = require("./src/middlewares/interceptor")
+
+const authApi = require("./src/routes/auth");
+const managersApi = require("./src/routes/managers");
+const masterApi = require("./src/routes/master");
+const notificationsApi = require("./src/routes/notifications");
+const schedulesApi = require("./src/routes/schedules");
+const usersApi = require("./src/routes/users");
+const utillsApi = require("./src/routes/utills");
+
+const app = express();
+const port = process.env.HTTP_PORT;
+
+require("dotenv").config();
+
+app.use(express.json());
 app.use(errorHandler);
+app.use(interceptor);
+
+app.use("/auth", authApi);
+app.use("/managers", managersApi);
+app.use("/master", masterApi);
+app.use("/notifications", notificationsApi);
+app.use("/schedules", schedulesApi);
+app.use("/users", usersApi);
+app.use("/", utillsApi);
+
+app.listen(port, () => {
+    console.log(`${port}번에서 HTTP Web Server 실행`);
+})
