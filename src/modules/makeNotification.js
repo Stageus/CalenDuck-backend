74636a70132d118b8cfaff424d632450f16074db
yunseo <yunseo10987@naver.com>
2024-06-25 16:58:46 +0900

    feat: "알림 기능 추가"
    알림 모듈 추가

---
+++ src/modules/makeNotification.js
@@ -0,0 +1,30 @@
+const notificationSchema = require("../../database/mongooseSchema/loggingSchema");
+
+const makeNotification = async (receiverIdx, type, data) => {
+  if (type === "import") {
+    await notificationSchema.create({
+      type: type,
+      receiver: receiverIdx,
+      interest: data.interest,
+      contents: data.contents,
+      is_read: false,
+    });
+  } else if (type === "manager") {
+    await notificationSchema.create({
+      type: type,
+      receiver: receiverIdx,
+      interest: data.interest,
+      is_read: false,
+    });
+  } else if (type === "reply") {
+    await notificationSchema.create({
+      type: type,
+      receiver: receiverIdx,
+      title: data.title,
+      reply: data.reply,
+      is_read: false,
+    });
+  }
+};
+
+module.exports = makeNotification;
