ee386140f8190c523829d38122f50c7d1fd54e39
SuinLee21 <suin7473@naver.com>
2024-06-26 11:38:29 +0900

    refactor: "순서 수정"

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const errorHandler = require("./src/middlewares/errorHandler");
 const interceptor = require("./src/middlewares/interceptor");
+const notFoundApi = require("./src/middlewares/notFoundApi");
 
 const authApi = require("./src/routes/auth");
 const managersApi = require("./src/routes/managers");
@@ -10,15 +11,10 @@ const notificationsApi = require("./src/routes/notifications");
 const schedulesApi = require("./src/routes/schedules");
 const usersApi = require("./src/routes/users");
 const utillsApi = require("./src/routes/utills");
-const errorHandler = require("./src/middlewares/errorHandler");
-const notFoundApi = require("./src/middlewares/notFoundApi");
-
-const app = express();
-const port = process.env.HTTP_PORT;
 
 require("dotenv").config();
-
-app.use(interceptor);
+const app = express();
+const port = process.env.HTTP_PORT;
 
 app.use("/auth", authApi);
 app.use("/managers", managersApi);
@@ -28,6 +24,7 @@ app.use("/schedules", schedulesApi);
 app.use("/users", usersApi);
 app.use("/", utillsApi);
 
+app.use(interceptor);
 app.use(notFoundApi);
 app.use(errorHandler);
 
