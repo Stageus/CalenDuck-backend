38b73da5cde7867130969db55e1aacae95570c17
yunseo <yunseo10987@naver.com>
2024-06-26 16:52:45 +0900

    feat: "Mongoose 기능 추가"
    mongoose connect 메소드 추가

--- database/connect/mongodb.js
+++ database/connect/mongodb.js
@@ -1,18 +1,17 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const uri = process.env.MONGODB_URI;
-const connectMongoDB = async () => {
-    try {
-        const client = await MongoClient.connect(uri, {
-            maxPoolSize: process.env.MONGODB_MAX
-        });
-        console.log("Connected");
-        return client.db("calenduck");
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
-}
 
-module.exports = connectMongoDB;
+const connectMongoose = () => {
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("mongoose connected");
+    })
+    .catch((err) => {
+      console.log("mongoose connection fail", err);
+    });
+};
+
+module.exports = connectMongoose;
