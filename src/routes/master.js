77f514ed048a7b68b5da8f47e300bab3bffc17fa
SuinLee21 <suin7473@naver.com>
2024-06-30 02:42:34 +0900

    feat: "/asks 기능 추가"

--- src/routes/master.js
+++ src/routes/master.js
@@ -1,25 +1,25 @@
 const router = require("express").Router();
 
-const { InternalServerError } = require("../model/customError");
+const {
+    BadRequestError,
+    InternalServerError
+} = require("../model/customError");
 
 const psql = require("../../database/connect/postgre");
 
 router.get("/users", async (req, res, next) => {
-    const result = {
-        "data": null
-    }
-
     try {
-        const selectUserQueryResult = await psql.query(`
+        const userData = await psql.query(`
             SELECT idx, name FROM calenduck.user
         `);
 
-        if (selectUserQueryResult.rows.length === 0) {
+        if (userData.rows.length === 0) {
             return res.sendStatus(204);
         }
 
-        result.data = selectUserQueryResult.rows;
-        return res.sendStatus(200);
+        return res.status(200).send({
+            data: userData.rows
+        });
     } catch (err) {
         console.log(err);
         return next(new InternalServerError("Internal Server Error"));
@@ -27,47 +27,70 @@ router.get("/users", async (req, res, next) => {
 })
 
 router.get("/interest", async (req, res, next) => {
-    const result = {
-        "data": null
-    }
-
     try {
-        const selectInterestQueryResult = await psql.query(`
+        const interestData = await psql.query(`
             SELECT idx, interest FROM calenduck.interest
             WHERE is_assigned=false
         `)
 
-        if (selectInterestQueryResult.rows.length === 0) {
+        if (interestData.rows.length === 0) {
             return res.sendStatus(204);
         }
 
-        result.data = selectInterestQueryResult.rows;
-        return res.sendStatus(200);
+        return res.status(200).send({
+            data: interestData.rows
+        });
     } catch (err) {
         console.log(err);
         return next(new InternalServerError("Internal Server Error"));
     }
 })
 
-router.get("/users/interest-admin", async (req, res) => {
-    const result = {
-        "data": null
-    }
-
+router.get("/users/interest-admin", async (req, res, next) => {
     try {
         const managerWithInterest = await psql.query(`
             SELECT CM.user_idx, CM.interest_idx, CI.idx, CI.interest
             FROM calenduck.manager CM
-            INNER JOIN calenduck.interest CI
-            ON CM.interest_idx=CI.idx; 
+            JOIN calenduck.interest CI
+            ON CM.interest_idx = CI.idx; 
         `);
 
         if (managerWithInterest.rows.length === 0) {
             return res.sendStatus(204);
         }
 
-        result.data = managerWithInterest.rows;
-        return res.sendStatus(200);
+        return res.status(200).send({
+            data: managerWithInterest
+        });
+    } catch (err) {
+        console.log(err);
+        return next(new InternalServerError("Internal Server Error"));
+    }
+})
+
+router.get("/asks", async (req, res, next) => {
+    const { categoryIdx } = req.query;
+
+    if (!categoryIdx) {
+        return next(new BadRequestError("cannot find query"));
+    }
+
+    try {
+        const askWithUser = await psql.query(`
+            SELECT CA.idx, CA.title, CA.contents, CA.reply, CA.created_at, CU.name
+            FROM calenduck.ask CA
+            JOIN calenduck.user CU
+            ON CA.user_idx=CU.idx
+            WHERE CA.ask_category_idx = $1; 
+        `);
+
+        if (askWithUser.rows.length === 0) {
+            return res.sendStatus(204);
+        }
+
+        return res.status(200).send({
+            data: askWithUser
+        });
     } catch (err) {
         console.log(err);
         return next(new InternalServerError("Internal Server Error"));
