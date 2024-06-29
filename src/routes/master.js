3978103ae9d0775f47d017bfa87886aa198af658
SuinLee21 <suin7473@naver.com>
2024-06-29 14:33:21 +0900

    feat: "/users/interest-admin 기능 추가"

--- src/routes/master.js
+++ src/routes/master.js
@@ -11,7 +11,7 @@ router.get("/users", async (req, res, next) => {
 
     try {
         const selectUserQueryResult = await psql.query(`
-            SELECT idx, name FROM calenduck.userr
+            SELECT idx, name FROM calenduck.user
         `);
 
         if (selectUserQueryResult.rows.length === 0) {
@@ -22,7 +22,7 @@ router.get("/users", async (req, res, next) => {
         return res.sendStatus(200);
     } catch (err) {
         console.log(err);
-        return next(new InternalServerError("Internet Server Error"));
+        return next(new InternalServerError("Internal Server Error"));
     }
 })
 
@@ -45,7 +45,32 @@ router.get("/interest", async (req, res, next) => {
         return res.sendStatus(200);
     } catch (err) {
         console.log(err);
-        return next(new InternalServerError("Internet Server Error"));
+        return next(new InternalServerError("Internal Server Error"));
+    }
+})
+
+router.get("/users/interest-admin", async (req, res) => {
+    const result = {
+        "data": null
+    }
+
+    try {
+        const managerWithInterest = await psql.query(`
+            SELECT CM.user_idx, CM.interest_idx, CI.idx, CI.interest
+            FROM calenduck.manager CM
+            INNER JOIN calenduck.interest CI
+            ON CM.interest_idx=CI.idx; 
+        `);
+
+        if (managerWithInterest.rows.length === 0) {
+            return res.sendStatus(204);
+        }
+
+        result.data = managerWithInterest.rows;
+        return res.sendStatus(200);
+    } catch (err) {
+        console.log(err);
+        return next(new InternalServerError("Internal Server Error"));
     }
 })
 
