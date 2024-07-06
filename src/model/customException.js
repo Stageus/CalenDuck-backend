bf8164e8faf5db3b5d8570025d786b3b8713c32a
yunseo <yunseo10987@naver.com>
2024-07-06 16:09:28 +0900

    feat: "customExcpetion 생성자 메시지 입력 기능 추가"
    메시지가 없을 경우 기본 메시지 생성

--- src/model/customException.js
+++ src/model/customException.js
@@ -11,40 +11,40 @@ class customException {
 }
 
 class BadRequestException extends customException {
-  constructor(err = null) {
-    super(400, "입력한 데이터가 올바르지 않습니다.", err);
+  constructor(message = null ,err = null) {
+    super(400, message || "입력한 데이터가 올바르지 않습니다.", err);
   }
 }
 
 class UnauthorizedException extends customException {
-  constructor(err = null) {
-    super(401, "인증되지 않은 접근입니다.", err);
+  constructor(message = null, err = null) {
+    super(401, message || "인증되지 않은 접근입니다.", err);
   }
 }
 
 class ForbiddenException extends customException {
-  constructor(err = null) {
-    super(403, "접근 권한이 없습니다.", err);
+  constructor(message = null, err = null) {
+    super(403, message || "접근 권한이 없습니다.", err);
   }
 }
 
 class NotFoundException extends customException {
-  constructor(err = null) {
-    super(404, "요청하신 정보를 찾을 수 없습니다.", err);
+  constructor(message = null, err = null) {
+    super(404, message || "요청하신 정보를 찾을 수 없습니다.", err);
   }
 }
 
 class ConflictException extends customException {
-  constructor(err = null) {
-    super(409, "동일한 값이 이미 데이터베이스에 존재합니다.", err);
+  constructor(message = null, err = null) {
+    super(409, message || "동일한 값이 이미 데이터베이스에 존재합니다.", err);
   }
 }
 
 class TooManyRequestsException extends customException {
-  constructor(err = null) {
+  constructor(message = null, err = null) {
     super(
       429,
-      "요청 횟수 제한을 초과하였습니다. 잠시 후 다시 시도해 주세요.",
+      message || "요청 횟수 제한을 초과하였습니다. 잠시 후 다시 시도해 주세요.",
       err
     );
   }
