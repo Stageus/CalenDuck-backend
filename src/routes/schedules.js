cd04272ac56fc758e6cbfd84fd7ce686a1facbd3
blue-ssom <somi5404@naver.com>
2024-07-06 17:12:33 +0900

    feat : "특정 년월 특정 관심사 불러오기 기능 추가"

--- src/routes/schedules.js
+++ src/routes/schedules.js
@@ -73,4 +73,44 @@ router.get("/", async (req, res, next) => {
     }
 })
 
+// 특정 년월 특정 관심사 불러오기
+router.get("/interest", async (req, res, next) => {
+    const { date, interestIdx } = req.query;
+
+    try{
+        const year = date.substring(0, 4);
+        const month = date.substring(4, 6);
+
+        // 날짜별로 빈 리스트 초기화(31개)
+        let scheduleList = Array.from({ length: 31 }, () => []);
+
+        // 관심사 스케줄 가져오기
+        const interest_schedule = await getManyResults(`
+            SELECT COUNT(*) AS count, EXTRACT(DAY FROM time) as day, contents as interestName
+            FROM calenduck.interest_schedule
+            WHERE EXTRACT(YEAR FROM time) = $1 AND EXTRACT(MONTH FROM time) = $2 AND interest_idx = $3
+            GROUP BY EXTRACT(DAY FROM time), contents
+        `, [year, month, interestIdx]);
+
+        if ( interest_schedule === 0 ) {
+            return res.sendStatus(204); 
+        }
+    
+        // 관심사 스케줄을 날짜별로 리스트에 추가
+        interest_schedule.forEach(schedule => {
+            const day = schedule.day - 1; // index값이 day 값은 같게 하기 위해서 1을 뺌
+            scheduleList[day].push({
+                interestName: schedule.interestname,
+                count: schedule.count
+            });
+        });
+
+        return res.status(200).json({
+            list: scheduleList
+        });
+    }catch(err){
+        return next(err);
+    }
+})
+
 module.exports = router;
