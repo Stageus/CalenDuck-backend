1e28e19e3a8c3d7c09b20be6c8e7d56ecc6511b6
SuinLee21 <suin7473@naver.com>
2024-07-09 01:31:10 +0900

    feat: "상수화 작업"

---
+++ src/model/constants.js
@@ -0,0 +1,30 @@
+// checkValidity 정규식
+const idRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,12}$/; // 영어 + 숫자, 각 최소 1개 이상, 6~12
+const pwRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[a-zA-Z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,16}$/; // 영어 + 숫자 + 특수문자, 각 최소 1개 이상, 8~16
+const emailRegex = /^(?!\.)(?!.*\.\.)[a-zA-Z\d.!#$%&'*+/=?^_{|}~-]+(?<!\.)@(?!-)(?!.*--)(?=.{1,253}$)([a-zA-Z\d-]{1,63}(?:\.[a-zA-Z\d-]{1,63})*(?<!-)\.[a-zA-Z]{2,6})$/
+const nameRegex = /^[a-zA-Zㄱ-ㅎ가-힣]{2,32}$/;
+const whitespaceRegex = /[\r\n\t ]+/g;
+const paramRegex = /^(?!0)[\d]+$/
+const codeRegex = /^[\d]{6}$/;
+
+// /notification
+const pageSize = 20;
+
+// /auth
+const min = 100000;
+const max = 999999;
+const range = max - min + 1;
+
+module.exports = {
+    idRegex,
+    pwRegex,
+    emailRegex,
+    nameRegex,
+    whitespaceRegex,
+    paramRegex,
+    codeRegex,
+    pageSize,
+    min,
+    max,
+    range
+};
