7ad9519e0c5520375589b48636660d7fd1da7e0d
SuinLee21 <suin7473@naver.com>
2024-07-08 18:06:32 +0900

    style: "변수 수정 및 삭제"

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -7,18 +7,18 @@ const {
 
 const checkAuth = (type = null) => {
     return (req, res, next) => {
-        const { token } = req.cookies.access_token;
+        const token = req.cookies.access_token;
 
         try {
             const jwtData = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
             req.decoded = jwtData;
 
-            const validTypes = ["signup", "findId", "findPw", "resetPw", "login"];
+            const validTypes = ["signup", "findId", "findPw", "login"];
 
             if (validTypes.includes(type) && jwtData.type !== type) {
-                return next(new ForbiddenException());
+                return next(new UnauthorizedException());
             }
-            if (type === "admin" && jwtData.admin !== true) {
+            if (type === "admin" && jwtData.rank !== "admin") {
                 return next(new ForbiddenException());
             }
             return next();
