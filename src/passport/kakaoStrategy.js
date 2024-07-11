762b6b9293021ed95ca3a532dba1a8fb92a31b99
SuinLee21 <suin7473@naver.com>
2024-07-11 18:34:50 +0900

    feat: "직렬화 및 역직렬화 기능 추가"

--- src/passport/kakaoStrategy.js
+++ src/passport/kakaoStrategy.js
@@ -3,6 +3,28 @@ const KakaoStrategy = require("passport-kakao").Strategy;
 
 const psql = require("../../database/connect/postgre");
 
+const makeToken = require("../modules/makeToken");
+
+// 사용자 직렬화 설정
+passport.serializeUser((user, done) => {
+    done(null, user.id); // 사용자 ID를 세션에 저장
+});
+
+// 사용자 역직렬화 설정
+passport.deserializeUser(async (id, done) => {
+    try {
+        const result = await psql.query(`
+            SELECT *
+            FROM calenduck.kakao
+            WHERE id = $1
+        `, [id]);
+
+        done(null, result.rows[0]); // 사용자 객체를 복원
+    } catch (err) {
+        done(err);
+    }
+});
+
 module.exports = () => {
     passport.use(
         new KakaoStrategy(
@@ -12,23 +34,27 @@ module.exports = () => {
             },
 
             async (accessToken, refreshToken, profile, done) => {
-                console.log("kakao profile", profile);
                 try {
                     const exUser = await psql.query(`
-                        SELECT idx FROM backend.kakao WHERE member_num = $1
+                        SELECT *
+                        FROM calenduck.kakao
+                        WHERE id = $1
                     `, [profile.id]);
                     // 이미 가입된 카카오 프로필이면 성공
-                    if (exUser) {
-                        done(null, exUser); // 로그인 인증 완료
+                    if (exUser.rows.length !== 0) {
+                        done(null, exUser.rows[0]); // 로그인 인증 완료
                     } else {
                         // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
                         const newUser = await psql.query(`
-                            INSERT INTO backend.kakao(member_num) VALUES($1)
+                            INSERT INTO calenduck.kakao(id)
+                            VALUES($1)
+                            RETURNING *
                         `, [profile.id]);
-                        done(null, newUser); // 회원가입하고 로그인 인증 완료
+
+                        makeToken({ "email": profile._json.kakao_account.email });
+                        done(null, newUser.rows[0]); // 회원가입하고 로그인 인증 완료
                     }
                 } catch (error) {
-                    console.error(error);
                     done(error);
                 }
             }
