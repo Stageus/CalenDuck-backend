c9c2229bdef2406e027ba713397ad90e9b46141c
blue-ssom <somi5404@naver.com>
2024-07-13 15:10:45 +0900

    refactor : "List 변수 선언을 const로 변경 및 if문 수정"

--- src/routes/schedules.js
+++ src/routes/schedules.js
@@ -24,7 +24,7 @@ router.get("/", checkAuth("login"), endRequestHandler(async (req, res, next) => 
     const month = date.substring(4, 6);
 
     // 날짜별로 빈 리스트 초기화(31개)
-    let scheduleList = Array.from({ length: 31 }, () => []);
+    const scheduleList = Array.from({ length: 31 }, () => []);
 
     // 개인 스케줄 가져오기
     const personalScheduleList = await getManyResults(`
@@ -42,7 +42,7 @@ router.get("/", checkAuth("login"), endRequestHandler(async (req, res, next) => 
         GROUP BY EXTRACT(DAY FROM time), contents
     `, [year, month]);
 
-    if ( personalScheduleList.length === 0 && !interestScheduleList.length === 0 ) return res.sendStatus(204); 
+    if ( personalScheduleList.length === 0 && interestScheduleList.length === 0 ) return res.sendStatus(204); 
 
     // 개인 스케줄을 날짜별로 리스트에 추가
     personalScheduleList.forEach(schedule => {
