dec5ebaa2bf8dd0cdfeabf9a48c72a62a2d6ea88
yunseo <yunseo10987@naver.com>
2024-07-18 15:03:38 +0900

    refactor: "서버 에러 시, console.error 구문으로 로그"

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -6,7 +6,7 @@ const errorHandler = async (err, req, res, next) => {
       message: err.message,
     });
   } else {
-    console.log(err);
+    console.error(err);
 
     return res.status(500).send({
       message: "서버 에러 발생",
