31d06ccfa4eff0e994c8fffd76738c6c8253e731
SuinLee21 <suin7473@naver.com>
2024-08-23 16:35:04 +0900

    refactor: "checkValidity 매개변수 수정"

--- src/routes/interests.js
+++ src/routes/interests.js
@@ -13,15 +13,19 @@ const {
 } = require("../modules/sqlHandler");
 const endRequestHandler = require("../modules/endRequestHandler");
 
-const { LOGIN } = require("../constants");
+const {
+  PARAM_REGEX,
+  LOGIN
+} = require("../constants");
 
-// 관심사 목록 불러오기
-router.get("/all", checkAuth(LOGIN), endRequestHandler(async (req, res, next) => {
+// 관리자 할당된 관심사 목록 불러오기
+router.get("/all", checkAuth("master"), endRequestHandler(async (req, res, next) => {
   const interestList = await getManyResults(`
-        SELECT idx AS "interestIdx", interest AS "interestName"
-        FROM calenduck.interest
-        ORDER BY idx ASC
-    `);
+    SELECT idx AS "interestIdx", interest AS "interestName"
+    FROM calenduck.interest
+    WHERE is_assigned=true
+    ORDER BY interest ASC
+  `)
 
   if (interestList.length === 0) return res.sendStatus(204);
 
@@ -35,12 +39,12 @@ router.get("/", checkAuth(LOGIN), endRequestHandler(async (req, res, next) => {
   const loginUser = req.decoded;
 
   const interestList = await getManyResults(`
-      SELECT CI.interest AS "interestIdx", CI.idx AS "interestName" FROM calenduck.user_interest CUI
-      JOIN calenduck.interest CI
-      ON CUI.interest_idx = CI.idx
-      WHERE CUI.user_idx = $1
-      ORDER BY interest ASC
-    `, [loginUser.idx]);
+    SELECT CI.interest AS "interestIdx", CI.idx AS "interestName" FROM calenduck.user_interest CUI
+    JOIN calenduck.interest CI
+    ON CUI.interest_idx = CI.idx
+    WHERE CUI.user_idx = $1
+    ORDER BY interest ASC
+  `, [loginUser.idx]);
 
   if (!interestList || interestList.length === 0) return res.sendStatus(204);
 
@@ -51,29 +55,29 @@ router.get("/", checkAuth(LOGIN), endRequestHandler(async (req, res, next) => {
 );
 
 //관심사 추가
-router.post("/:idx", checkAuth(LOGIN), checkValidity({ "numberField": ["idx"] }), endRequestHandler(async (req, res, next) => {
+router.post("/:idx", checkAuth(LOGIN), checkValidity({ [PARAM_REGEX]: ["idx"] }), endRequestHandler(async (req, res, next) => {
   const { idx: interestIdx } = req.params;
   const loginUser = req.decoded;
 
   await psql.query(`
-      INSERT INTO calenduck.user_interest(user_idx, interest_idx) 
-      VALUES($1, $2)
-    `, [loginUser.idx, interestIdx]);
+    INSERT INTO calenduck.user_interest(user_idx, interest_idx) 
+    VALUES($1, $2)
+  `, [loginUser.idx, interestIdx]);
 
   return res.sendStatus(201);
 })
 );
 
 //내 관심사 삭제
-router.delete("/:idx", checkAuth(LOGIN), checkValidity({ "numberField": ["idx"] }), endRequestHandler(async (req, res, next) => {
+router.delete("/:idx", checkAuth(LOGIN), checkValidity({ [PARAM_REGEX]: ["idx"] }), endRequestHandler(async (req, res, next) => {
   const { idx: interestIdx } = req.params;
   const loginUser = req.decoded;
 
   const userInterest = await getOneResult(`
-      DELETE FROM calenduck.user_interest 
-      WHERE user_idx = $1 AND interest_idx = $2
-      RETURNING interest_idx
-    `, [loginUser.idx, interestIdx]);
+    DELETE FROM calenduck.user_interest 
+    WHERE user_idx = $1 AND interest_idx = $2
+    RETURNING interest_idx
+  `, [loginUser.idx, interestIdx]);
 
   if (!userInterest) return next(new NotFoundException());
 
